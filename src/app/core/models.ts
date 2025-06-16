dbcbf7225d4197d3f225d6d055f679f4ce5e6238
Sule Malik <abdulmaliksuleman75@gmail.com>
2025-06-16 10:55:00 +0000

    initial commit

---
+++ src/app/core/models.ts
@@ -0,0 +1,12 @@
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+
+  // Optional properties for UI simulation
+  category?: string;
+  date?: string;
+  readTime?: string;
+  image?: string;
+}
