491fcee807ee01fa2e79cc2c1aeb83c1a3934995
Sule Malik <abdulmaliksuleman75@gmail.com>
2025-06-19 11:41:16 +0000

    complete auth simulations as well us upgraded styles and catching and also security implementations with envs

---
+++ src/custom-typings/core/validators/profanity.validator.spec.ts
@@ -0,0 +1,7 @@
+import { Profanity } from './profanity.validator';
+
+describe('Profanity', () => {
+  it('should create an instance', () => {
+    expect(new Profanity()).toBeTruthy();
+  });
+});
---
+++ src/custom-typings/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://jsonplaceholder.typicode.com',
+  localPostKey: 'localPosts',
+  overridePostKey: 'postOverrides',
+  cacheDurationMs: 1000 * 60 * 5 // 5 minutes
+};
